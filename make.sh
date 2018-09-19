1e96b30886a7cf9707ce36a07c1b4dbca0c67fd4
Ash Hall <ashwhall@gmail.com>
2018-09-19 13:43:36 +1000

    Make script, decribed imagenet

---
+++ make.sh
@@ -0,0 +1,4 @@
+echo \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n | pdflatex thesis;
+biber thesis;
+echo \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n | pdflatex thesis;
+
